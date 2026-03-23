336b436a55abeccc67ba304d4a625e33fa017cf4
tsitohainassandatia <hei.sandatia@gmail.com>
2026-03-23 18:46:33 +0300

    installation de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./html/**/*.html",        
+    "./script/**/*.js",        
+    "./tokimahery.data.js",   
+    "./index.html"             
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
